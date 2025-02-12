e706ebea5c7a20fc0a446e844cd9886356b1d1de
Solomon Chika <solomon.chika1@gmail.com>
2025-02-12 23:14:04 +0100

    FTT Touch Up

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -7,7 +7,7 @@ export const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKE
 export const WEBHOOK_BASE_URL = process.env.WEBHOOK_BASE_URL || 'https://betestsolom.pagekite.me'; // Replace with your domain 
 export const BOT_LINK = process.env.BOT_LINK || '';
 export const FRONTEND_PAYMENT_URL = process.env.FRONTEND_PAYMENT_URL || '';
-export const TELEGRAM_CHANNEL_ID = process.env.TELEGRAM_CHANNEL_ID || '@freetutorbottest';
+export const TELEGRAM_CHANNEL_ID = process.env.TELEGRAM_CHANNEL_ID || -2400975853;
 
 // Tron Network Configuration
 export const TRON_FULL_NODE = process.env.TRON_FULL_NODE || 'https://api.trongrid.io';
@@ -19,4 +19,4 @@ export const MERCHANT_ADDRESS = process.env.NEXT_PUBLIC_MERCHANT_TRON_ADDRESS ||
 export const USDT_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_USDT_CONTRACT_ADDRESS || 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t'; // USDT Contract on Tron
 
 // Optional: Private key for signing transactions
-export const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+export const PRIVATE_KEY = process.env.PRIVATE_KEY || '0x0000000000000000000000000000000000000000000000000000000000000000';
