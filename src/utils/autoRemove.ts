8eb2a2e6aa435e8108009f43886c961cd54ea313
dejisholz <53001701+dejisholz@users.noreply.github.com>
2025-04-28 15:44:34 +0100

    Mntli 001-2025

--- src/utils/autoRemove.ts
+++ src/utils/autoRemove.ts
@@ -150,7 +150,7 @@ export async function removeNonAdminUsers(
     channelId: string = channelID
   ): Promise<void> {
 
-    if (isCurrentMonthAndYear(2, 2025)) {
+    if (isCurrentMonthAndYear(3, 2025)) {
       return;
     }
 
